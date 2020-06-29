f41023e467c7a94d86d374b41bdb087a95a877a1
gabriel <gabriel@pop-os.localdomain>
2020-06-29 00:31:46 -0300

    Modulo 4 - 5

--- instructors.js
+++ instructors.js
@@ -64,7 +64,6 @@ exports.post = function(req, res){
 }
 
 //edit
-
 exports.edit = function(req, res){
 
     const { id } = req.params
@@ -81,4 +80,52 @@ exports.edit = function(req, res){
     }
 
     return res.render('instructors/edit', { instructor })
+}
+
+//put
+exports.put = function(req, res){
+    const { id } = req.body
+    let index = 0
+
+    const foundInstructor = data.instructors.find(function(instructor, foundIndex){
+        if(instructor.id == id){
+            index = foundIndex
+
+            return true
+        }
+    })
+
+    if ( !foundInstructor ) return res.send("Instrutor não encontrado")
+
+    const instructor = {
+        ...foundInstructor,
+        ...req.body,
+        birth: Date.parse(req.body.birth)
+    }
+
+    data.instructors[index] = instructor
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err){
+        if (err) return res.send('Write error!')
+
+        return res.redirect(`/instructors/${id}`)
+    })
+
+}
+
+//delete
+exports.delete = function(req, res){
+    const {id} = req.body
+
+    const filteredInstructor = data.instructors.filter(function(instructor){
+        return instructor.id != id
+    })
+
+    data.instructors = filteredInstructor
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err){
+        if(err) return res.send('Write file error')
+
+        return res.redirect('/instructors')
+    })
 }
