f48f54a61fa66df2386634b6df423d2452b9933d
gabriel <gabriel@pop-os.localdomain>
2020-06-30 18:11:07 -0300

    função marcar menu

--- public/scripts.js
+++ public/scripts.js
@@ -1,15 +1,9 @@
-const trs = document.querySelectorAll('.lines')
-let id = 0
-for (tr of trs) {
-    id = id + 1
-    let elements =  document.getElementById(id).textContent.split(',')
-    console.log(elements)
-    document.getElementById(id).innerHTML = ""
-    for (element of elements){
-        console.log(document.getElementById(id).innerHTML)
-        document.getElementById(id).innerHTML += ` <span>${element}</span>`
+const currentPage = location.pathname
+const menuItem = document.querySelectorAll('header .links a')
 
-    }
-
+for ( item of menuItem) {
+    if(currentPage.includes(item.getAttribute('href')))
+        item.classList.add('active')
 }
+
 
